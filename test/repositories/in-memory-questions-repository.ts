cef79d06ba94d2371a69801596ba10f5f0b49348
esbnet <esbnet@gmail.com>
2023-08-30 17:53:34 -0300

    feat: :sparkles: find a question by slug

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,7 +1,19 @@
-import { QuestionsRepository } from '@/domain/forum/application/repositories/question-repositories'
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 import { Question } from '@/domain/forum/enterprise/entities/question'
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
+  async findBySlug(slug: string) {
+    const question = this.questions.find(
+      (question) => question.slug.value === slug,
+    )
+
+    if (!question) {
+      return null
+    }
+
+    return question
+  }
+
   private questions: Question[] = []
   async create(question: Question) {
     this.questions.push(question)
