98d85acd88faf317c6c8694a6ae5fa9942c17760
esbnet <esbnet@gmail.com>
2023-08-31 15:48:30 -0300

    feat: :sparkles: create factories tests - question

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -4,15 +4,22 @@ import {
   Question,
 } from '@/domain/forum/enterprise/entities/question'
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
+import { faker } from '@faker-js/faker'
 
-export function makeQuestion(override: Partial<IQuestionProps> = {}) {
-  const question = Question.create({
-    title: 'Example Question',
-    slug: Slug.create('example-question'),
-    authorId: new UniqueEntityID('1'),
-    content: 'Example content',
-    ...override,
-  })
+export function makeQuestion(
+  override: Partial<IQuestionProps> = {},
+  id?: UniqueEntityID,
+) {
+  const question = Question.create(
+    {
+      title: faker.lorem.sentence(),
+      slug: Slug.create('example-question'),
+      authorId: new UniqueEntityID('1'),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  )
 
   return question
 }
