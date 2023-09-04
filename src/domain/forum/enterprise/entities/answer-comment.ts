f4afb7d909de349079628f230788ca2d6ad83d20
esbnet <esbnet@gmail.com>
2023-09-04 15:38:32 -0300

    feat: :sparkles:

--- src/domain/forum/enterprise/entities/answer-comment.ts
+++ src/domain/forum/enterprise/entities/answer-comment.ts
@@ -1,43 +1,14 @@
-import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
+import { Comment, CommentProps } from './comment'
 
-export interface IAnswerCommentProps {
-  authorId: UniqueEntityID
+export interface IAnswerCommentProps extends CommentProps {
   answerId: UniqueEntityID
-  content: string
-  createdAt: Date
-  updatedAt?: Date
 }
 
-export class AnswerComment extends Entity<IAnswerCommentProps> {
-  get content() {
-    return this.props.content
-  }
-
-  set content(content: string) {
-    this.props.content = content
-    this.touch()
-  }
-
-  get authorId() {
-    return this.props.authorId
-  }
-
+export class AnswerComment extends Comment<IAnswerCommentProps> {
   get answerId() {
     return this.props.answerId
-  }
-
-  get createdAt() {
-    return this.props.createdAt
-  }
-
-  get updatedAt() {
-    return this.props.updatedAt
-  }
-
-  private touch() {
-    this.props.updatedAt = new Date()
   }
 
   static create(
