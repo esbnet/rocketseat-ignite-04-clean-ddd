646ee669f49ba430e6dbec974d1c364c7bf64456
esbnet <esbnet@gmail.com>
2023-09-09 17:44:27 -0300

    feat: :sparkles: create delete question attachments

--- test/repositories/in-memory-question-attachments-repository.ts
+++ test/repositories/in-memory-question-attachments-repository.ts
@@ -1,6 +1,5 @@
 import { QuestionAttachmentsRepository } from '@/domain/forum/application/repositories/question-attachments-repository'
 import { QuestionAttachment } from '@/domain/forum/enterprise/entities/question-attachment'
-
 export class InMemoryQuestionAttachmentsRepository
   implements QuestionAttachmentsRepository
 {
@@ -12,5 +11,13 @@ export class InMemoryQuestionAttachmentsRepository
     )
 
     return questionAttachments
+  }
+
+  async deleteManyByQuestionId(questionId: string) {
+    const questionAttachments = this.items.filter(
+      (item) => item.questionId.toString() !== questionId,
+    )
+
+    this.items = questionAttachments
   }
 }
