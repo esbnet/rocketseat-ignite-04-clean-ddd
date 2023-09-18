320230aef8283146801564b50c030a5761af0328
esbnet <esbnet@gmail.com>
2023-09-18 20:01:40 -0300

    feat: :sparkles: create estructure for events of domain

--- src/core/entities/aggregate-root.ts
+++ src/core/entities/aggregate-root.ts
@@ -1,3 +1,25 @@
 // eslint-disable-next-line prettier/prettier
-import { Entity } from './entity';
-export abstract class AggregateRoot<Props> extends Entity<Props> {}
+import { DomainEvent } from '../events/domain-event'
+import { DomainEvents } from '../events/domain-events'
+import { Entity } from './entity'
+
+export abstract class AggregateRoot<Props> extends Entity<Props> {
+  clearEvents() {
+    throw new Error('Method not implemented.')
+  }
+
+  private _domaninEvents: DomainEvent[] = []
+
+  get domaninEvents(): DomainEvent[] {
+    return this._domaninEvents
+  }
+
+  get domainEvents(): DomainEvent[] {
+    return this._domaninEvents
+  }
+
+  protected addDomainEvent(domainEvent: DomainEvent): void {
+    this._domaninEvents.push(domainEvent)
+    DomainEvents.markAggregateForDispatch(this)
+  }
+}
